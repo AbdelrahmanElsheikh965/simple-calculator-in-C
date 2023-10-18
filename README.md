4e276479b989f2b58afc97f55bc8ae6a86db1481
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2023-10-18 22:26:38 +0300

    Bug fixes & update README file
    1. Handled more cases like unexpected input (letters).
    2. Cleared the buffer
    3. Updated README file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simple-calculator-in-C
+
+This is a simple console application in C to simulate simple calculator.
+
+
+The program takes 3 input from the user:
+1. First number
+2. Second number
+3. An operation to perform on these number
+
+Almost every case was handled and, if there's anything not handled yet feel free to comment or let me know and I'll try to be working more and more to enhance my implementations.
+
+The user enter the first number, the program checks it as number or not and, the second one finally, the third one is the operation and it's done.
--- main.c
+++ main.c
@@ -8,43 +8,55 @@ float divide(int, int);
 
 int main()
 {
-    // write a C Program to make simple calculations (add, sub, multiply, divide) on two numbers
+        // write a C Program to make simple calculations (add, sub, multiply, divide) on two numbers.
 
-    int firstNum, secondNum;
-    char operation;
+        int firstNum, secondNum;
+        char operation;
 
-    printf("Type the first number and, press enter: \n");
-    scanf("%d", &firstNum);
+        printf("Type the first number and, press enter: \n");
 
-    printf("Type the second number and, press enter: \n");
-    scanf("%d", &secondNum);
+        // check if it's taken as expected from user (as integer), if not return 0.
+        if(0 != scanf("%d", &firstNum))
+        {
+            printf("Type the second number and, press enter: \n");
 
-    printf("Choose the operation symbol you want to perform (+, -, *, /) and, press enter: \n");
-    scanf(" %c", &operation);
+            if(0 != scanf("%d", &secondNum))
+            {
+                printf("Choose the operation symbol you want to perform (+, -, *, /) and, press enter: \n");
+                while(getchar() != '\n');
+                if(0 != scanf("%c", &operation))
+                    {
+                        switch(operation){
+                            case '+':
+                                printf("Result of Add Operation is =  %i \n\n", add(firstNum, secondNum));
+                                break;
 
-    switch(operation){
-        case '+':
-            printf("Result of Add Operation is =  %i \n\n", add(firstNum, secondNum));
-            break;
+                            case '-':
+                                printf("Result of Add Operation is =  %i \n\n", subtract(firstNum, secondNum));
+                                break;
 
-        case '-':
-            printf("Result of subtract Operation is =  %i \n\n", subtract(firstNum, secondNum));
-            break;
+                            case '*':
+                                printf("Result of Add Operation is =  %i \n\n", multiply(firstNum, secondNum));
+                                break;
 
-        case '*':
-            printf("Result of multiply Operation is =  %i \n\n", multiply(firstNum, secondNum));
-            break;
+                            case '/':
+                                if (divide(firstNum, secondNum) == -1){
+                                    printf("Division by Zero Error \n\n");
+                                }else{
+                                    printf("Result of division Operation is =  %f \n\n", divide(firstNum, secondNum));
+                                }
+                                break;
 
-        case '/':
-            if (divide(firstNum, secondNum) == -1){
-                printf("Error Division by Zero \n\n");
+                            default:
+                                printf("Error, Invalid Operation . . . \n\n");
+                                break;
+                            }
+                    }
             }else{
-                printf("Result of divide Operation is =  %f \n\n", divide(firstNum, secondNum));
+                 printf("Invalid Second Number");
             }
-            break;
-
-        default:
-            printf("Error, Try Again . . . \n\n");
+        }else{
+            printf("Invalid First Number");
         }
 
     return 0;
@@ -70,5 +82,4 @@ float divide(int firstNum, int secondNum)
         return -1;
     else
         return (double)firstNum / (double)secondNum;
-
 }
